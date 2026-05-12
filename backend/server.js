2c5d24c6add2db039254f1f90e7df350a645248e
M0nss12 <monsswhat@gmail.com>
2026-05-12 14:04:45 +0800

    Pochta

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT || 465,
-    secure: true, 
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
